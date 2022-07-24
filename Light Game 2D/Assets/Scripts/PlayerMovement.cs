b724605329dd366877316c8a770ae1f93d394225
uday-kumar7202 <akhilrocx007@gmail.com>
2022-07-24 17:53:01 +0530

    Initial Commit

---
+++ Light Game 2D/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private float horizontalInput;
+    private float speed = 5f;
+    private float jumpingPower = 9f;
+    private bool IsFacingRight = true;
+
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        if(Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+        }
+
+        if(Input.GetButtonUp("Jump") && rb.velocity.y < 0f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+        }
+
+        Flip();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2 (horizontalInput * speed, rb.velocity.y);
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+
+    private void Flip()
+    {
+        if(IsFacingRight && horizontalInput > 0f || !IsFacingRight && horizontalInput < 0f)
+        {
+            IsFacingRight = !IsFacingRight;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+        }
+    }
+}
